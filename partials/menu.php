cd73d073b622e0c2a9d410b4d5e393b8126fc957
elvircn <elvircrn@gmail.com>
2016-09-16 02:16:55 +0200

    nije responsive (jos)

--- partials/menu.php
+++ partials/menu.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="http://jobfair.ba/"><img class="logo-img" src="img/logo/jf_logo.png" style="margin-left:-30px;"></a>
+            <a class="navbar-brand" href="http://jobfair.ba/"><img class="logo-img" src="img/logo/jf_logo.png" style="margin-left:-30px;margin-top:-30px;"></a>
         </div>
 
 
