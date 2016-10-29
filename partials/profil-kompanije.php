5540ba29e1ebf91367cb8190a8fbc10d38e11f21
Mustafa Mahmutovic <mmahmutovi1@etf.unsa.ba>
2016-10-30 01:38:44 +0200

    webić

--- partials/profil-kompanije.php
+++ partials/profil-kompanije.php
@@ -6,7 +6,7 @@
 
 	<div class="container-fluid" style="margin-top:13%!important">
 	<div class="profil-podaci" >
-        <a href="http://api.jobfair.ba/static/kompanije/<?=$item->web;?>"><img src="http://api.jobfair.ba/static/kompanije/<?=$item->id;?>.png" style="width:250px; margin-left:18%" class="slika-kompanije" alt="API"></a>
+        <a href="<?=$item->web;?>"><img src="http://api.jobfair.ba/static/kompanije/<?=$item->id;?>.png" style="width:250px; margin-left:18%" class="slika-kompanije" alt="API"></a>
         <div>
             
 
