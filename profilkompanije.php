0d2651016b554cddb96f27a30a9b4be4a1ed6b36
Mustafa Mahmutovic <mmahmutovi1@etf.unsa.ba>
2016-10-29 23:36:12 +0200

    m

--- profilkompanije.php
+++ profilkompanije.php
@@ -3,7 +3,7 @@
  //$url_home = 'http://www.jobfair.ba/';
  //$url_home = "http://www.jobfair.ba/";
  $url_home = 'http://localhost/jobfair16/';
- $item = json_decode(file_get_contents('http://api.jobfair.ba/api/kompanije/'.$_GET['id']));
+ $item = json_decode(file_get_contents('http://api.jobfair.ba/participants/'.$_GET['id']));
 
  //if(!isset($item->naslov)) header('Location: '.$url_home.'404');
 
